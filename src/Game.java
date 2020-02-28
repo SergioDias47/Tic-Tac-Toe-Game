1be168e6eff1d09e0c376f360827ab8ccaf9f2ac
SergioDias47 <47751859+SergioDias47@users.noreply.github.com>
2020-02-28 21:15:58 +0000

    Finished implementing AI

--- src/Game.java
+++ src/Game.java
@@ -34,7 +34,7 @@ public class Game implements ActionListener {
 	}
 	
 	public boolean checkWinner() {
-		gameOver = true;
+		//gameOver = true;
 		winner = (playerTurn % 2) + 1;
 		
 		//Check rows
@@ -60,8 +60,47 @@ public class Game implements ActionListener {
 			return true;
 		
 		winner = 0;
-		gameOver = false;
+		//gameOver = false;
 		return false;
+	}
+	
+	public boolean checkWinner(int player) {
+		//gameOver = true;
+		winner = (playerTurn % 2) + 1;
+		
+		//Check rows
+		if(cells[1] == cells[2] && cells[2] == cells[3] && cells[3] == player)
+			return true;
+		if(cells[4] == cells[5] && cells[5] == cells[6] && cells[6] == player)
+			return true;
+		if(cells[7] == cells[8] && cells[8] == cells[9] && cells[9] == player)
+			return true;
+
+		//Check columns
+		if(cells[1] == cells[4] && cells[4] == cells[7] && cells[7] == player)
+			return true;
+		if(cells[2] == cells[5] && cells[5] == cells[8] && cells[8] == player)
+			return true;
+		if(cells[3] == cells[6] && cells[6] == cells[9] && cells[9] == player)
+			return true;
+		
+		//Check diagonals
+		if(cells[1] == cells[5] && cells[5] == cells[9] && cells[9] == player)
+			return true;
+		if(cells[7] == cells[5] && cells[5] == cells[3] && cells[3] == player)
+			return true;
+		
+		winner = 0;
+		//gameOver = false;
+		return false;
+	}
+	
+	public int getScore() {
+		if(checkWinner(MyConstants.PLAYER_1))
+			return MyConstants.X_SCORE;
+		if(checkWinner(MyConstants.PLAYER_2))
+			return MyConstants.O_SCORE;
+		else return MyConstants.TIE;
 	}
 	
 	public void nextPlayer() {
@@ -69,9 +108,12 @@ public class Game implements ActionListener {
 	}
 	
 	public int updateGameState(int pos) { // returns 0 if failure, 1 if player 1 played and same for 2
-		if(cells[pos] != 0 || gameOver)
+		if(isGameOver())
+			return 0;
+		if(cells[pos] != MyConstants.EMPTY_CELL || gameOver)
 			return 0;
 		int currentPlayer = playerTurn;
+		System.out.println(currentPlayer + "   " + pos);
 		cells[pos] = currentPlayer;
 		nextPlayer();
 		return currentPlayer;
@@ -87,7 +129,6 @@ public class Game implements ActionListener {
 
 	@Override
     public void actionPerformed(ActionEvent e) {
-		System.out.println(((JComponent)e.getSource()).getName());
 		if(e.getSource().getClass() == TTTButton.class) {
 			int button = Integer.parseInt(((JComponent) e.getSource()).getName());
 			int ret = updateGameState(button);
@@ -105,6 +146,8 @@ public class Game implements ActionListener {
 			}
 			
 			if(gamemode == MyConstants.PLAYER_VS_BOT) {
+				if(isGameOver())
+					return;
 				int pos = findBestMove();
 				updateGameState(pos);
 				gui.fillBotCell(pos);
@@ -135,12 +178,50 @@ public class Game implements ActionListener {
     }
 
 	private int findBestMove() {
+		int bestScore = -Integer.MIN_VALUE;
+		int bestMove = 0; // position in the grid
 		for(int i = 1; i <= 9; i++)
-			if(cells[i] == MyConstants.EMPTY_CELL) 
-				return i;
-		return 0;
+			if(cells[i] == MyConstants.EMPTY_CELL) {
+				cells[i] = MyConstants.O_CELL;
+				int score = minimax(false);
+				if(score > bestScore) {
+					bestScore = score;
+					bestMove = i;
+				}
+				cells[i] = MyConstants.EMPTY_CELL;
+			}
+		return bestMove;
+	}
+
+	private int minimax(boolean isMaximizing) {
+		int currentScore = getScore();
+		if(isGameOver()) {
+			return currentScore;
+		}
+		
+		if(isMaximizing) {
+			int bestScore = Integer.MIN_VALUE;
+			for(int i = 1; i <= 9; i++) {
+				if(cells[i] == MyConstants.EMPTY_CELL) {
+					cells[i] = MyConstants.O_CELL;
+					int score = minimax(false);
+					cells[i] = MyConstants.EMPTY_CELL;
+					bestScore = Integer.max(bestScore, score);
+				}
+			}
+			return bestScore;
+		} else {
+			int bestScore = Integer.MAX_VALUE;
+			for(int i = 1; i <= 9; i++) {
+				if(cells[i] == MyConstants.EMPTY_CELL) {
+					cells[i] = MyConstants.X_CELL;
+					int score = minimax(true);
+					cells[i] = MyConstants.EMPTY_CELL;
+					bestScore = Integer.min(bestScore, score);
+				}
+			}
+			return bestScore;
+		}
 	}		
 	
 }
-
-//https://github.com/OSSpk/Minesweeper-Desktop-Game/tree/master/Code/src/minesweeper
--- src/MyConstants.java
+++ src/MyConstants.java
@@ -1,8 +1,12 @@
-
 public class MyConstants {
 	public final static int PLAYER_VS_PLAYER = 0;
 	public final static int PLAYER_VS_BOT = 1;
 	public final static int EMPTY_CELL = 0;
 	public final static int X_CELL = 1;
 	public final static int O_CELL = 2;
+	public final static int TIE = 0;
+	public final static int PLAYER_1 = 1;
+	public final static int PLAYER_2 = 2;
+	public final static int O_SCORE = 10;
+	public final static int X_SCORE = -10;
 }
